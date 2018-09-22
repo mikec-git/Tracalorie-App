e186a0df002b65526cd6751eb4e3f191a0961c06
Mike Choi <mike.choi@live.ca>
2018-09-21 20:14:46 -0700

    Added functionality to show edit state for current selected item on UI

--- app.js
+++ app.js
@@ -38,11 +38,29 @@ const ItemCtrl = (function(){
 
         getTotalCalories: function(){
             let total = 0;
-            data.items.forEach((item) => {
+            data.items.forEach(item => {
                 total += item.calories;
             });
             data.totalCalories = total;
             return data.totalCalories;
+        },
+
+        getItemById: function(id){
+            let found = null;
+            data.items.forEach(item => {
+                if(item.id === id){
+                    found = item;
+                }
+            });
+            return found;
+        },
+
+        setCurrentItem: function(item){
+            data.currentItem = item;
+        },
+
+        getCurrentItem: function(){
+            return data.currentItem;
         }
     };
 })();
@@ -52,6 +70,9 @@ const UICtrl = (function(){
     const UISelectors = {
         itemList: '#item-list',
         addBtn: '.add-btn',
+        updateBtn: '.update-btn',
+        deleteBtn: '.delete-btn',
+        backBtn: '.back-btn',
         itemNameInput: '#item-name',
         itemCaloriesInput: '#item-calories',
         totalCalories: '.total-calories'
@@ -107,6 +128,26 @@ const UICtrl = (function(){
 
         showTotalCalories: function(totalCalories){
             document.querySelector(UISelectors.totalCalories).textContent = totalCalories;
+        },
+
+        clearEditState: function(){
+            this.clearFields();
+            document.querySelector(UISelectors.updateBtn).style.display = 'none';
+            document.querySelector(UISelectors.deleteBtn).style.display = 'none';
+            document.querySelector(UISelectors.backBtn).style.display   = 'none';
+            document.querySelector(UISelectors.addBtn).style.display    = 'inline';
+        },
+        
+        showEditState: function(){
+            document.querySelector(UISelectors.updateBtn).style.display = 'inline';
+            document.querySelector(UISelectors.deleteBtn).style.display = 'inline';
+            document.querySelector(UISelectors.backBtn).style.display   = 'inline';
+            document.querySelector(UISelectors.addBtn).style.display    = 'none';
+        },
+
+        addItemToForm: function(currentItem){
+            document.querySelector(UISelectors.itemNameInput).value = currentItem.name;
+            document.querySelector(UISelectors.itemCaloriesInput).value = currentItem.calories;
         }
     };
 })();
@@ -119,9 +160,10 @@ const App = (function(ItemCtrl, UICtrl){
         const UISelectors = UICtrl.getSelectors();
 
         document.querySelector(UISelectors.addBtn).addEventListener('click', itemAddSubmit);
+        document.querySelector(UISelectors.itemList).addEventListener('click', itemUpdateSubmit);
     }
 
-    itemAddSubmit = function(e){
+    const itemAddSubmit = function(e){
         // Get form input from UI Controller
         const {itemNameInput: itemName, itemCaloriesInput: itemCalories} = UICtrl.getItemInput();
         
@@ -138,10 +180,26 @@ const App = (function(ItemCtrl, UICtrl){
 
         e.preventDefault();
     }
+
+    const itemUpdateSubmit = function(e){
+        if(e.target.classList.contains('edit-item')){
+            const itemId = e.target.parentNode.parentNode.id.split('-')[1];
+            const itemToEdit = ItemCtrl.getItemById(parseInt(itemId));
+            ItemCtrl.setCurrentItem(itemToEdit);
+            UICtrl.addItemToForm(ItemCtrl.getCurrentItem());
+            UICtrl.showEditState();
+        }
+        
+        e.preventDefault();
+    }
     
     return {
         init: function(){
+            // Set Initial State
+            UICtrl.clearEditState();
             console.log('Initializing app...');
+
+            // Fetch items in list
             const items = ItemCtrl.getItems();
 
             // Check if any items in list
